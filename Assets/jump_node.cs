b71e4735e3996c61154729bb30c429b1b570bd1b
Sukrit Tan <actsukrit@gmail.com>
2020-09-14 21:23:26 +0700

    Created Jump Nodes; Modified Simple Jump for Compat.

---
+++ Assets/jump_node.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump_node : MonoBehaviour
+{
+
+	//==================================================
+	// Properties
+	//==================================================
+
+	public List<jump_node> child_nodes = new List<jump_node>();
+	protected List<jump_node> parent_nodes = new List<jump_node>();
+
+	//==================================================
+	// Standard Unity Methods
+	//==================================================
+	void Start()
+	{
+		// Add self to childrens' parent node list
+		foreach(jump_node child_node in child_nodes){
+			child_node.parent_nodes.Add(this);
+		}
+	}
+	void Update()
+	{
+	}
+	//==================================================
+	// Unity Editor Methods
+	//==================================================
+	void OnDrawGizmos(){
+		float R = (transform.position.x % 5.0f)/5.0f;
+		float G = (transform.position.y % 5.0f)/5.0f;
+		float B = (transform.position.z % 5.0f)/5.0f;
+		Gizmos.color = new Color(R, G, B, 1.0f);
+
+		Debug.Log(Gizmos.color);
+
+		Gizmos.DrawSphere(transform.position, 0.5f);
+		foreach(jump_node child_node in child_nodes){
+			Gizmos.DrawLine(transform.position, child_node.transform.position);
+		}
+	}
+	//==================================================
+	// Custom Methods
+	//==================================================
+	public jump_node GetRandomNode(bool forward = true){
+		List<jump_node> nodes = forward ? child_nodes : parent_nodes;
+		int num_nodes  = nodes.Count;
+
+		if (num_nodes < 1) return null;
+		return nodes[Random.Range(0, num_nodes)];
+	}
+
+	public bool IsDeadEnd(bool forward = true){
+		List<jump_node> nodes = forward ? child_nodes : parent_nodes;
+		int num_nodes  = nodes.Count;
+
+		return (num_nodes < 1);
+	}
+}
--- Assets/simple_jump.cs
+++ Assets/simple_jump.cs
@@ -4,67 +4,53 @@ using UnityEngine;
 
 public class simple_jump : MonoBehaviour
 {
-    private enum State { Floor, S1, S2, S3, S4, S5 }
-    [SerializeField] private State _state;
-    [SerializeField] private GameObject _stair;
-    [SerializeField] private GameObject _floor;    
-    
-    public float jumpUpVelocity;   // Velocity on y-axis
 
+	//==================================================
+	// Properties
+	//==================================================
+	public float jumpUpVelocity;   // Velocity on y-axis
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //  1
-        _state = State.Floor;         
-    }
+	[SerializeField] private jump_node current_node;
+	private bool forward;
+	//==================================================
+	// Standard Unity Methods
+	//==================================================
+	void Start()
+	{
+		forward = !(current_node.IsDeadEnd(true));
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // stateAction();
+	void Update()
+	{
+		if(Mathf.Approximately(GetComponent<Rigidbody>().velocity.y,  0.0f)){
+			 stateAction();
+		 }
+	}
+	//==================================================
+	// Custom Methods
+	//==================================================
+	void stateAction() {
 
-        //  5
-        if(Mathf.Approximately(GetComponent<Rigidbody>().velocity.y,  0.0f)){
-             stateAction();
-         }
+		if (current_node.IsDeadEnd(forward)) forward = !forward;
 
+		jump_node new_node = current_node.GetRandomNode(forward);
+		jumpTo(new_node.transform.position);
+		current_node = new_node;
+	}
 
-        // Final
-        //if(Mathf.Approximately(GetComponent<Rigidbody>().velocity.y,  0.0f) && Input.GetButtonDown("Jump") == true){
-        //    stateAction();
-        //}
-    }
+	void jumpTo(Vector3 point){
+		Vector3 direction = point - transform.position;
+		float time = timeToLand(point);
+		float vX = direction.x / time;
+		float vZ = direction.z / time;
 
+		GetComponent<Rigidbody>().velocity = new Vector3(vX, jumpUpVelocity, vZ);
+	}
 
-    // 2
-    void stateAction() {
-
-        while (Mathf.Approximately(GetComponent<Rigidbody>().velocity.y, 0.0f))
-        {
-            _state = (State)Random.Range(0, 5);
-            string S = _state.ToString();
-            jumpTo(_stair.transform.Find(S).position);
-        }
-    }
-    
-
-
-    //  3
-    void jumpTo(Vector3 point){
-        Vector3 direction = point - transform.position;
-        float time = timeToLand(point);
-        float vX = direction.x / time;
-        float vZ = direction.z / time;
-
-        GetComponent<Rigidbody>().velocity = new Vector3(vX, jumpUpVelocity, vZ);
-    }
-
-    //  4
-    float timeToLand(Vector3 point){
-        float g = Physics.gravity.y;
-        float sqrt = Mathf.Sqrt(Mathf.Abs(jumpUpVelocity * jumpUpVelocity - 2 * g * (transform.position - point).y ));
-        return Mathf.Min(-jumpUpVelocity + sqrt, -jumpUpVelocity - sqrt ) / g;
-    }
+	float timeToLand(Vector3 point){
+		float g = Physics.gravity.y;
+		float sqrt = Mathf.Sqrt(Mathf.Abs(jumpUpVelocity * jumpUpVelocity - 2 * g * (transform.position - point).y ));
+		return Mathf.Min(-jumpUpVelocity + sqrt, -jumpUpVelocity - sqrt ) / g;
+	}
 
 }
