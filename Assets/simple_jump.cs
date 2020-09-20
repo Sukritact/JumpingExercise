1a15707fd28fe89290fd1736b2721f5cd58d3ff5
Sukritact_Mahidol <actsukrit@gmail.com>
2020-09-20 10:44:05 +0700

    Changed Scene. Changed Jumping to Projectile Calc

--- Assets/simple_jump.cs
+++ Assets/simple_jump.cs
@@ -9,6 +9,7 @@ public class simple_jump : MonoBehaviour
 	// Properties
 	//==================================================
 	public float jumpUpVelocity;   // Velocity on y-axis
+	private float angle = 80;
 
 	[SerializeField] private jump_node current_node;
 	private bool forward;
@@ -42,17 +43,30 @@ public class simple_jump : MonoBehaviour
 
 	void jumpTo(Vector3 point){
 		Vector3 direction = point - transform.position;
-		float time = timeToLand(point);
-		float vX = direction.x / time;
-		float vZ = direction.z / time;
+		float height = direction.y;
+		direction.y = 0;
+		float dist = direction.magnitude;
+		float a = angle * Mathf.Deg2Rad;
+		direction.y = dist * Mathf.Tan(a);
+		dist += height / Mathf.Tan(a);
+		float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
 
-		GetComponent<Rigidbody>().velocity = new Vector3(vX, jumpUpVelocity, vZ);
+		GetComponent<Rigidbody>().velocity = velocity * direction.normalized;
 	}
 
-	float timeToLand(Vector3 point){
-		float g = Physics.gravity.y;
-		float sqrt = Mathf.Sqrt(Mathf.Abs(jumpUpVelocity * jumpUpVelocity - 2 * g * (transform.position - point).y ));
-		return Mathf.Min(-jumpUpVelocity + sqrt, -jumpUpVelocity - sqrt ) / g;
-	}
+	// void jumpTo(Vector3 point){
+	// 	Vector3 direction = point - transform.position;
+	// 	float time = timeToLand(point);
+	// 	float vX = direction.x / time;
+	// 	float vZ = direction.z / time;
+
+	// 	GetComponent<Rigidbody>().velocity = new Vector3(vX, jumpUpVelocity, vZ);
+	// }
+
+	// float timeToLand(Vector3 point){
+	// 	float g = Physics.gravity.y;
+	// 	float sqrt = Mathf.Sqrt(Mathf.Abs(jumpUpVelocity * jumpUpVelocity - 2 * g * (transform.position - point).y ));
+	// 	return Mathf.Min(-jumpUpVelocity + sqrt, -jumpUpVelocity - sqrt ) / g;
+	// }
 
 }
